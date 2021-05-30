0e7c3b98fe94d04e97091cd649ce192ea3f0d07f
qianyu <280624745@qq.com>
2021-05-30 16:06:07 +0800

    修改Footer.vue内容

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,46 +1,28 @@
 <template>
   <div class="footer">
-    <div class="remind">
-      <h3>提别提醒：以下内容为占位符，用于日后补充信息，可忽略</h3>
-    </div>
     <div class="content">
-      <div>message1</div>
-      <div>message2</div>
-      <div>message3</div>
-      <div>message4</div>
-      <div>message5</div>
-      <div>message6</div>
-      <div>message7</div>
-      <div>message8</div>
-      <div>message9</div>
-      <div>message10</div>
-      <div>message11</div>
-      <div>message12</div>
-      <div>message13</div>
-      <div>message14</div>
-      <div>message15</div>
+      <div>Vue wheel UI -Vue3</div>
+      <div>Vue wheel UI -React</div>
+      <div>GitHub</div>
+      <div>Gitee</div>
+      <div>Vue3</div>
+      <div>Vue Router</div>
+      <div>Vite</div>
+      <div>MDN WebDocs</div>
+      <div>Ant Design of Vue</div>
+      <div>Vant</div>
+      <div>CSDN</div>
+      <div>W3school</div>
+      <div>知乎</div>
+      <div>掘金</div>
     </div>
   </div>
 </template>
-
 
 <style lang="scss" scoped>
 .footer {
   background: rgba(0, 0, 0, 0.9);
   color: rgba(255, 255, 255, 0.8);
-
-  > .remind {
-    padding: 50px 120px 0 120px;
-    @media (max-width: 600px) {
-      padding: 50px 20px 0 20px;
-      > h3 {
-        font-size: 16px;
-      }
-    }
-    @media (max-width: 350px) {
-      padding: 30px 20px 0 20px;
-    }
-  }
 
   > .content {
     padding: 50px 120px;
@@ -48,12 +30,9 @@
     grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
     grid-template-columns: 1fr 1fr 1fr;
     @media (max-width: 600px) {
-      padding: 50px 30px;
-      grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-      grid-template-columns: 1fr 1fr;
-    }
-    @media (max-width: 350px) {
       padding: 30px;
+      grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+      grid-template-columns: 1fr;
     }
 
     > div {
